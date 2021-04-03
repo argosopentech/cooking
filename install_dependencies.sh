ad013a0a8f96ada4684ea3e10c7d90027fa229b5
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-03 20:15:01 +0000

    Site running

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,2 +1,2 @@
 sudo apt-get install -y vim git docker.io
-sudo apt-get install -y discount # Needed for running based.cooking make files
+# sudo apt-get install -y discount # Needed for running based.cooking make files
