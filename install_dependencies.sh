a9e2f198f5b1e8910815613bfe6441c403a94de8
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-03 19:49:22 +0000

    Added scripts

---
+++ install_dependencies.sh
@@ -0,0 +1,2 @@
+sudo apt-get install -y vim git docker.io
+sudo apt-get install -y discount # Needed for running based.cooking make files
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # Requires docker
 
-docker build -t some-content-nginx .
-docker run --name some-nginx -d -p 80:80 some-content-nginx
+sudo docker build -t some-content-nginx .
+sudo docker run --name some-nginx -d -p 80:80 some-content-nginx
